af8cff76f0a58b325df0646e84d5e7dd5e9a02ca
Sachin Ganesh <finolex2009@gmail.com>
2017-09-16 13:21:43 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var content={
+var articleOne={
     title: 'Article One | Sachin Ganesh',
     heading: 'Article One',
     date: 'Sep 5,2016',
@@ -22,6 +22,46 @@ var content={
         <p>
             Blah Blah Blah Blah Blah Blah Blah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah BlahBlah Blah Blah
         </p> `
+};
+
+function createTemplate (data){
+    var title = data.title;
+    var date = data.date;
+    var heading = data.heading;
+    var content = data.content;
+    
+var htmlTemplate = `
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial=scale=1"
+/>
+<link href="/ui/style.css" rel="stylesheet" />
+</head>
+    <body>
+        <div class="container">
+        <div>
+            <a href="/">Home</a>
+        </div>
+        <hr/>
+        <h3>
+            ${heading}
+        </h3>
+        <div>
+            ${date}
+        </div>
+        <div>
+    ${content}
+        </div>
+        </div>
+    </body>
+</html>
+
+
+`;
+return htmlTemplate;
 }
 
 app.get('/', function (req, res) {
